bf2074eeb2b2d62f291466b215c8abcc0ce67309
Lojose <lojoselopezm@gmail.com>
2017-10-19 15:34:46 -0700

    Update List.cpp

--- List.cpp
+++ List.cpp
@@ -1,7 +1,10 @@
 #include "List.h"
 #include "Iterator.h"
+#include <list>
 #include <cassert>
+#include <iostream> //Testing Purposes
 
+using namespace std; //Testing Purposes
 List::List () {
 	Listfront = nullptr;
 	Listback = nullptr;  
@@ -14,12 +17,7 @@ bool List::empty() const {
 	return Listfront == nullptr;
 }
 
-void List::Prepend( Link *x) {
-	if (Listback = nullptr) {
-		Prepend(Listfront->next); 
-	}
-}
-void List::push_back (int val){
+void List::push_back (int val) {
 	Link *pt1 = new Link (val);
 	if (empty()) {
 		Listfront = Listback = pt1;
@@ -28,13 +26,12 @@ void List::push_back (int val){
 			if (cur1 == nullptr) Listback = cur1; 
 			pt1 = Listback; 
 		}
-		Link *update = new Link(val); 
-		Listfront->next = update; 
-		Listfront = update; 
+		Link *update1 = new Link(val); 
+		Listfront->next = update1; 
+		Listfront = update1; 
 	}
 	n++; 
 }
-	
 
 void List::push_front(int val) {
 	Link *pt2 = new Link(val); 
@@ -42,19 +39,59 @@ void List::push_front(int val) {
 		Listback = Listfront = pt2; 
 	}
 	else {
-		Listfront->next = pt2; 
-		Listfront = pt2; 
+		for (Link *cur2 = Listfront; cur2 != nullptr; cur2 = cur2->next) {
+			if (cur2 == nullptr) Listfront = cur2; 
+			cur2 = Listfront; 
+		}
+		Link *update2 = new Link(val); 
+		Listback->next = update2; 
+		Listback = update2; 
 	}
 	n++; 
 }
+
+void List::prepop_back(int val) {
+	Link *pt3 = new Link(val);
+	Listback->next = pt3;
+	Listback = pt3;
+	delete pt3;
+}
+
 void List::pop_back() {
-	Link *p = Listback; 
-	delete p; 
-	Listfront->next = Listback; 
+	prepop_back(0); 
 	++x; 
 }
+void 
 
 int List::size() {
 	n = n - x; 
 	return n; 
  }
+
+int main() {
+	List a; 
+	a.push_back(5); 
+	cout << "test1" << endl;
+	//cout << Listfront << endl;
+	a.push_back(4); 
+	cout << "test2" << endl;
+	a.push_back(23); 
+	cout << "test3" << endl;
+	a.push_front(10); 
+	cout << "test4" << endl;
+	a.push_front(13); 
+	cout << "test5" << endl;
+	a.push_front(0); 
+	cout << "test6" << endl; 
+	cout << a.size() << endl; 
+	a.pop_back();
+	cout << a.size() << endl;
+	a.pop_back();
+	a.pop_back();
+	a.pop_back();
+	a.pop_back();
+	a.pop_back();
+	a.pop_back();
+	a.pop_back();
+	cout << "All test passed." << endl; 
+}
